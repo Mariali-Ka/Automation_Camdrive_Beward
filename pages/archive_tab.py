05c07334fb1eb5c811c6b79958f9d39bf47f29c6
Mariali-Ka <ma.karkashenko@gmail.com>
2025-10-07 16:22:34 +0700

    addition to the archive folder

--- pages/archive_tab.py
+++ pages/archive_tab.py
@@ -1,6 +1,8 @@
 import re
 import time
 import random
+import calendar
+from datetime import datetime
 import allure
 from base.base_page import BasePage
 from config.links import Links
@@ -17,12 +19,14 @@ class ArchiveTab(BasePage):
     FIELD_CALENDAR = ("xpath", "//div[text()='Календарь']")
     OPENING_CALENDAR = ("xpath", "//img[@alt='Выберите дату']")
     OPENING_CALENDAR_MONTH_AGO =("xpath", "//a[@title='<Пред']")
-    CLICK_SELECTED_DATE = ("xpath", "//a[text()='13']")
+    SELECT_DAY_OPENING_CALENDAR = ("xpath", "//td[@data-handler='selectDay']")
     FIELD_INPUT_DATE = ("xpath", "//input[@id='download-date']")
     HEADING_C = ("xpath", "//th[@class='heading-c']")
     HEADING_l = ("xpath", "//th[@class='heading-l prev']")
     HEADING_R = ("xpath", "//th[@class='heading-r off']")
     HEADING_R_NEXT = ("xpath", "//th[@class='heading-r next']")
+    ALL_CELLS_WITH_DAYS_CALENDAR = ("xpath", "//div[@class='item day']")  # все ячейки с днями в календаре
+    ALL_CELLS_WITH_VIEWING_RECORDS = ("xpath", "//div[@class='time item  constant']")  # все ячейки с днями в календаре
 
     @allure.step("Go to archive tab")
     def click_go_to_archive_tab(self):
@@ -35,7 +39,15 @@ class ArchiveTab(BasePage):
 
     @allure.step("Go to one month ago calendar")
     def click_go_to_one_month_ago_calendar(self):
+        month_header = self.wait.until(EC.presence_of_element_located(self.HEADING_C))
+        current_month_text = month_header.text.strip()
+        print(f"Исходный месяц: {current_month_text}")
         self.wait.until(EC.element_to_be_clickable(self.HEADING_l)).click()
+        self.wait.until(lambda driver: self.wait.until(EC.presence_of_element_located(self.HEADING_C)).text.strip() != current_month_text)
+        new_month_header = self.wait.until(EC.presence_of_element_located(self.HEADING_C))
+        new_month_text = new_month_header.text.strip()
+        print(f"Новый месяц после нажатия: {new_month_text}")
+        assert new_month_text != current_month_text, "Месяц не изменился после нажатия кнопки"
 
     @allure.step("Go to one month advance calendar")
     def click_go_to_one_month_advance_calendar(self):
@@ -46,12 +58,63 @@ class ArchiveTab(BasePage):
         self.wait.until(EC.element_to_be_clickable(self.OPENING_CALENDAR)).click()
         assert self.wait.until(EC.visibility_of_element_located(self.OPENING_CALENDAR)).is_displayed()
 
+    allure.step("Selected date displayed field")
     def selected_date_displayed_field(self):
         self.wait.until(EC.element_to_be_clickable(self.OPENING_CALENDAR_MONTH_AGO)).click()
-        self.wait.until(EC.element_to_be_clickable(self.CLICK_SELECTED_DATE)).click()
+        # Получаем список активных дней
+        select_day_elements = self.wait.until(EC.visibility_of_all_elements_located(self.SELECT_DAY_OPENING_CALENDAR))
+        # Фильтруем только активные/валидные дни
+        valid_days = []
+        for day in select_day_elements:
+            # Проверка, что день не отключён
+            if "disabled" not in day.get_attribute("class") and day.text.strip().isdigit():
+                valid_days.append(day)
+        # Выводим доступные даты
+        print("Валидные дни:", [d.text for d in valid_days])
+        # Рандомно устанавливаем дату
+        random_valid_days = random.choice(select_day_elements)
+        random_valid_days.click()
+        time.sleep(5)
+        # Проверяем, что дата установилась в поле
         selected_date = self.wait.until(EC.visibility_of_element_located(self.FIELD_INPUT_DATE)).get_attribute("value")
+        # Проверяем формат даты
         assert re.match(r"\d{2}\.\d{2}\.\d{4}", selected_date)
 
+    allure.step("Viewing from camera")
+    def viewing_from_camera(self):
+        day_elements = self.wait.until(EC.visibility_of_all_elements_located(self.ALL_CELLS_WITH_DAYS_CALENDAR))
+
+        # Фильтруем только активные/валидные дни
+        valid_days = []
+        for day in day_elements:
+            # Пример: проверка, что день не отключён
+            if "disabled" not in day.get_attribute("class") and day.text.strip().isdigit():
+                valid_days.append(day)
+        print("Валидные дни:", [d.text for d in valid_days])
+
+        random_valid_days = random.choice(day_elements)
+        random_valid_days.click()
+
+        try:
+
+            records = self.wait.until(EC.visibility_of_all_elements_located(self.ALL_CELLS_WITH_VIEWING_RECORDS))
+
+            if not records:
+                print("Записи не найдены!")
+            else:
+                print(f"Найдено записей: {len(records)}")
+
+                # Выбираем случайную запись
+                random_record = random.choice(records)
+
+                # Прокручиваем к элементу (на случай, если он вне экрана)
+                self.driver.execute_script("arguments[0].scrollIntoView(true);", random_record)
+                random_record.click()
+                time.sleep(5)
+                print("Перешли к просмотру записи.")
+
+        finally:
+            pass
 
 
 
--- test/archive_tab_test.py
+++ test/archive_tab_test.py
@@ -8,6 +8,15 @@ from base.base_test import BaseTest
 class TestArchiveTab(BaseTest):
 
     @pytest.mark.smoke
+    def test_days_of_week_displayed_correctly_calendar(self):
+        self.authorization_page.open()
+        self.authorization_page.enter_login(self.login.LOGIN)
+        self.authorization_page.enter_password(self.login.PASSWORD)
+        self.authorization_page.click_enter_button()
+        self.archive_tab.click_go_to_archive_tab()
+
+
+    @pytest.mark.smoke
     def test_turn_over_calendar(self):
         self.authorization_page.open()
         self.authorization_page.enter_login(self.login.LOGIN)
@@ -18,6 +27,7 @@ class TestArchiveTab(BaseTest):
         self.archive_tab.click_go_to_one_month_advance_calendar()
         self.archive_tab.is_opened_archive_tab()
 
+    @pytest.mark.smoke
     def test_opening_calendar(self):
         self.authorization_page.open()
         self.authorization_page.enter_login(self.login.LOGIN)
@@ -26,7 +36,7 @@ class TestArchiveTab(BaseTest):
         self.archive_tab.click_go_to_archive_tab()
         self.archive_tab.click_checking_opening_calendar()
 
-
+    @pytest.mark.smoke
     def test_selected_date_displayed_field(self):
         self.authorization_page.open()
         self.authorization_page.enter_login(self.login.LOGIN)
@@ -35,4 +45,13 @@ class TestArchiveTab(BaseTest):
         self.archive_tab.click_go_to_archive_tab()
         self.archive_tab.click_checking_opening_calendar()
         self.archive_tab.selected_date_displayed_field()
+
+    @pytest.mark.smoke
+    def test_viewing_from_camera(self):
+        self.authorization_page.open()
+        self.authorization_page.enter_login(self.login.LOGIN)
+        self.authorization_page.enter_password(self.login.PASSWORD)
+        self.authorization_page.click_enter_button()
+        self.archive_tab.click_go_to_archive_tab()
+        self.archive_tab.viewing_from_camera()
      
